49751405c9c27bf62aa68a40230ebe000ca7793c
lordsynergy <djordj88@mail.ru>
2017-09-30 18:35:03 +0300

    Add encrypt password and authenticate to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,53 @@
+require 'openssl'
+
 class User < ApplicationRecord
+  # параметры работы модуля шифрования паролей
+  ITERATIONS = 20000
+  DIGEST = OpenSSL::Digest::SHA256.new
+
+  has_many :questions
+
+  validates :email, :username, presence: true
+  validates :email, :username, uniqueness: true
+
+  attr_accessor :password
+
+  validates_presence_of :password, on: :create
+
+  validates_confirmation_of :password
+
+  before_save :encrypt_password
+
+  def encrypt_password
+    if self.password.present?
+      # добавляем соль для усложнения пароля
+      self.password_salt = User.hash_to_string(OpenSSL::Random.random_bytes(16))
+
+      # создаем хэш пароля - уникальная строка, из которой невозможно восстановить
+      # исходный пароль
+      self.password_hash = User.hash_to_string(
+        OpenSSL::PKCS5.pbkdf2_hmac(self.password, self.password_salt, ITERATIONS, DIGEST.length, DIGEST
+        ))
+    end
+  end
+
+  # служебный метод, преобразующий бинарную строку в 16-ричный формат, для удобства хранения
+  def self.hash_to_string(password_hash)
+    password_hash.unpack('H*')[0]
+  end
+
+  # медот для аутентификации пользователей
+  def self.authenticate(email, password)
+    user = find_by(email: email) # сначала находим пользователя по email
+
+    # ВНИМАНИЕ: сравнивается только password_hash, а оригинальный пароль никогда
+    # нигде не сохраняется!
+    if user.present? && user.password_hash == User.hash_to_string(OpenSSL::PKCS5.pbkdf2_hmac(password,
+    user.password_salt, ITERATIONS, DIGEST.length, DIGEST))
+      user
+    else
+      nil
+    end
+  end
+
 end
