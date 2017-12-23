b6b8d30a12447a689163ba300e0fb9a4347f451f
lordsynergy <djordj88@mail.ru>
2017-12-23 21:55:25 +0300

    routes security fix

--- config/routes.rb
+++ config/routes.rb
@@ -10,14 +10,7 @@ Rails.application.routes.draw do
   # Ресурс вопросов (кроме экшенов :show, :new, :index)
   resources :questions, except: [:show, :new, :index]
 
-  # Ресурс сессий (только три экшена :new, :create, :destroy)
-  resources :sessions, only: [:new, :create, :destroy]
+  # Ресурс сессии (только три экшена :new, :create, :destroy)
+  resource :session, only: [:new, :create, :destroy]
 
-  # Синонимы путей — в дополнение к созданным в ресурсах выше.
-  #
-  # Для любознательных: синонимы мы добавили, чтобы показать одну вещь и потом
-  # их удалим.
-  get 'sign_up' => 'users#new'
-  get 'log_out' => 'sessions#destroy'
-  get 'log_in' => 'sessions#new'
 end
