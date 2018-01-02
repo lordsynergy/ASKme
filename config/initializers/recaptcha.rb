b55ee2b6cc3561e0657bc48233541ce462b78fe6
lordsynergy <djordj88@mail.ru>
2018-01-02 15:49:50 +0300

    fix2 recaptcha

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,5 +1,5 @@
 # Настройки гема recaptcha
 Recaptcha.configure do |config|
-  config.site_key  = ENV['RECAPTCHA_ASKME_PUBLIC_KEY']
-  config.secret_key = ENV['RECAPTCHA_ASKME_PRIVATE_KEY']
+  config.site_key  = ENV['RECAPTCHA_ASKME_SITE_KEY']
+  config.secret_key = ENV['RECAPTCHA_ASKME_SECRET_KEY']
 end
